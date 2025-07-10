5a0a69a952a29b4f9bdeebf62e028929af3aec58
c_not_around <10.dvd.01@gmail.com>
2025-07-10 11:15:40 +0300

    update scan

--- scan/scan.py
+++ scan/scan.py
@@ -16,6 +16,7 @@ FILE_FOOTER_SIZE   = 10800
 FRAME_HEADER_SIZE  = 512
 FRAME_DATA_SIZE    = 7452672
 FRAME_FOOTER_SIZE  = 36352
+FRAME_SIZE         = FRAME_HEADER_SIZE + FRAME_DATA_SIZE + FRAME_FOOTER_SIZE
 # offsets of duration in file header
 DURATION_OFFSETS   = [0x0BC8, 0x0C27, 0x0CEF, 0x0D6F, 0x15F2, 0x0E37, 0x0EB7, 0x0F7F, 0x0FFF, 0x10C7, 0x1147, 0x120F,
                       0x128F, 0x1357, 0x13D7, 0x1593, 0x16BA, 0x173A, 0x1802, 0x1882, 0x194A, 0x19CA, 0x1A92, 0x1B12,
@@ -36,72 +37,133 @@ def from_bcd(x):
 # convert to timestamp
 def to_timestamp(h, m, s, f):
     return f + 25 * (s + 60 * (m + 60 * h))
-# insert timestamp to buffer
-def set_timestamp(fs, ts):
-    buf = [0, 0, 0, 0]
-    for i in range(3, -1, -1):
-        buf[i] = ts
-        ts >>= 8
-        offset += 1
-    for o in TIMESTAMPS_OFFSETS:
-        fs.seek(o)
-        fs.write(buf)
+# insert value to buffer
+def set_value(fs, base, offsets, value, length):
+    buffer = [0 for i in range(length)]
+    for i in range(length-1, -1, -1):
+        buffer[i] = value & 0xFF
+        value >>= 8
+    for offset in offsets:
+        fs.seek(base+offset)
+        fs.write(bytes(buffer))
+# check key in block
+def check_key(fs, offset, key):
+    fs.seek(offset)
+    block = d.read(256)
+    if len(block) == 0:
+        return -1
+    if key_cmp(block, key):
+        return 1
+    return 0
+# find next full frame
+def scan_pass(fs, offset):
+    result = check_key(fs, offset, FRAME_HEADER_START)
+    if result == 1:
+        offset += FRAME_HEADER_SIZE
+        result = check_key(fs, offset, FRAME_DATA_START)
+        if result == 1:
+            offset += FRAME_DATA_SIZE
+            result = check_key(fs, offset, FRAME_FOOTER_START)
+    return result
+# write file header, file footer and set metadata to destination file
+def complete_file(fs, f_count, f_timestamp, f_fname):
+    pos = FILE_HEADER_SIZE + f_count * FRAME_SIZE
+    # file header
+    fs.seek(0)
+    fs.write(file_header)
+    set_value(fs, 0, DURATION_OFFSETS,   f_count,     4)
+    set_value(fs, 0, [0x002C],           pos,         8)
+    set_value(fs, 0, TIMESTAMPS_OFFSETS, f_timestamp, 4)
+    # file footer
+    fs.seek(pos)
+    fs.write(file_footer)
+    set_value(fs, pos, DURATION_OFFSETS,         f_count,     4)
+    set_value(fs, pos, [0x001C, 0x002C, 0x2A23], pos,         8)
+    set_value(fs, pos, TIMESTAMPS_OFFSETS,       f_timestamp, 4)
+    fs.close()
+    # log message
+    print("%i frames saved to file <%s>\r\n" % (f_count, f_fname))
 
-
-# load file header, file footer
-f = open("header.mxf", "rb")
-f.seek(0)
-file_header = f.read(FILE_HEADER_SIZE)
-f.close()
-f = open("footer.mxf", "rb")
-f.seek(0)
-file_footer = f.read(FILE_FOOTER_SIZE)
-f.close()
 
 # target disk select
-'''disks = ["%s:" % i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" if os.path.exists("%s:" % i)]
+disks = ["%s:" % i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" if os.path.exists("%s:" % i)]
 for d in range(len(disks)):
     print("%i - %s" % (d, disks[d]))
 d = int(input("select disk: "))
-disk = "\\\\.\\" + disks[d]'''
-disk = r"D:\temp\mxf\work\01707.mxf"
+disk = "\\\\.\\" + disks[d]
 
 # destinaton path select
 path = input("select destination path: ")
 
+# load file header, file footer
+fs = open("header.mxf", "rb")
+fs.seek(0)
+file_header = fs.read(FILE_HEADER_SIZE)
+fs.close()
+fs = open("footer.mxf", "rb")
+fs.seek(0)
+file_footer = fs.read(FILE_FOOTER_SIZE)
+fs.close()
+
 # scan cycle
 d = open(disk, "rb")
-offset = 0
-f_header_exists = False
-f_body_exists   = False
+offset      = 0
+f_count     = 0
+f_prev_no   = None
+f_timestamp = None
+f_fname     = None
 while True:
-    d.seek(offset)
-    block = d.read(256)
-    # end of disk space
-    if len(block) == 0:
+    r = scan_pass(d, offset)
+    # scan pass failed -> end of disk space
+    if r == -1:
         break
-    # check keys
-    if key_cmp(block, FRAME_HEADER_START):
+    # another frame found
+    if r == 1:
+        d.seek(offset)
+        buffer = d.read(FRAME_SIZE)
+        # get metadata
         n = 0
         for i in range(5):
             n <<= 8
-            n |= block[0x16+i]
-        Y = from_bcd(block[0x043])
-        M = from_bcd(block[0x042])
-        D = from_bcd(block[0x041])
-        h = from_bcd(block[0x040])
-        m = from_bcd(block[0x03F])
-        s = from_bcd(block[0x03E])
-        f = from_bcd(block[0x03D])
-        print("%016X: FRAME_HEADER no=%i %04i-%02i-%02i %02i:%02i:%02i.%02i" % (offset, n, Y, M, D, h, m, s, f))
-        f_header_exists = True
-    elif key_cmp(block, FRAME_DATA_START):
-        print("%016X: FRAME_DATA" % offset)
-        f_body_exists = True
-    elif key_cmp(block, FRAME_FOOTER_START):
-        print("%016X: FRAME_FOOTER" % offset)
-        
-    # to next block
-    offset += 256
+            n |= buffer[0x16+i]
+        Y = from_bcd(buffer[0x043])
+        M = from_bcd(buffer[0x042])
+        D = from_bcd(buffer[0x041])
+        h = from_bcd(buffer[0x040])
+        m = from_bcd(buffer[0x03F])
+        s = from_bcd(buffer[0x03E])
+        f = from_bcd(buffer[0x03D])
+        # complete file, if this frame is last in series
+        if (f_prev_no != None) and ((n-1) != f_prev_no):
+            complete_file(fs, f_count, f_timestamp, f_fname)
+            f_count   = 0
+            f_prev_no = None
+            f_fname   = None
+        # init first frame in current series
+        if f_count == 0:
+            f_timestamp = to_timestamp(h, m, s, f)
+            f_fname     = "%s\\%016X.mxf" % (path, offset)
+            fs = open(f_fname, "wb")
+        # copy frame to destinaton file
+        fs.seek(FILE_HEADER_SIZE+f_count*FRAME_SIZE)
+        fs.write(buffer)
+        # print frame info
+        print("%016X: FRAME no=%i %04i-%02i-%02i %02i:%02i:%02i.%02i" % (offset, n, Y, M, D, h, m, s, f*4))
+        # to next frame
+        f_count  += 1
+        f_prev_no = n
+        offset   += FRAME_SIZE
+    else:
+        # the sequence was interrupted -> complete file
+        if f_count != 0:
+            complete_file(fs, f_count, f_timestamp, f_fname)
+            f_count   = 0
+            f_prev_no = None
+            f_fname   = None
+        # jump to next block
+        offset += 256
+        # scan progress info
+        if (offset & 0x000000003FFFFFFF) == 0:
+            print("%iGb scaned." % (offset/(1024**3)))
 d.close()
 print("done.")
